6d49f6193358d2977773246a4bc40a3fdd8e3d17
Wilhelmus Tanujaya <wilhelm_andrean_22@yaho.com>
2016-04-21 06:09:59 +0700

    renaming and connection handler class

---
+++ Werewolf/src/TCPServer/ConnectionHandler.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TCPServer;
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author Hp
+ */
+public class ConnectionHandler implements Runnable{
+    private Socket server;
+    
+    public ConnectionHandler(Socket _server){
+        this.server = _server;
+    }
+    
+    @Override
+    public void run(){
+        try{
+            DataOutputStream out = new DataOutputStream(server.getOutputStream());
+            DataInputStream in = new DataInputStream(server.getInputStream());
+        
+            Boolean end = false;
+            while(!end){
+            
+            
+            }
+            server.close();
+        }catch(IOException e){
+            
+        }
+    }
+}
