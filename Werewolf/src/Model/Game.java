123d68b6009a2e1727f80aa38945c1c8dba1bfb1
fauzanriff <fauzanriff@gmail.com>
2016-05-08 13:38:35 +0700

    Fixing bug that 2nd proposer doesn't send kpu id

--- Werewolf/src/Model/Game.java
+++ Werewolf/src/Model/Game.java
@@ -106,7 +106,7 @@ public class Game {
     }
     
     public int checkLeader(){
-        if(preparatorLead.size() == playerSize()-1){
+        if(preparatorLead.size() == playerSize()-2){
             return makeLeader();
         }
         return -1;
