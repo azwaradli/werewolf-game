e170e68211126c177c1ab9f8bb65f088f6a1fef4
fauzanriff <fauzanriff@gmail.com>
2016-05-08 11:37:09 +0700

    Bug Fixes

--- Werewolf/src/Model/Game.java
+++ Werewolf/src/Model/Game.java
@@ -348,12 +348,15 @@ public class Game {
     }
     
     public boolean isGameOver(){
-        if(werewolfSize() == 0){
-            return true;
-        }else if(civilianSize() == 0){
-            return true;
-        }else if(civilianSize() == 1 && werewolfSize() == 1){
-            return true;
+        if(isStarted()){
+            if(werewolfSize() == 0){
+                return true;
+            }else if(civilianSize() == 0){
+                return true;
+            }else if(civilianSize() == 1 && werewolfSize() == 1){
+                return true;
+            }
+            return false;
         }
         return false;
     }
